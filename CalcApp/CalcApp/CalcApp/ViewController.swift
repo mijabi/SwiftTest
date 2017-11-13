82f3a9fecb293b245217738b27ac3daacb65062f
佐藤雅之 <sato-masay@mackbookPro.local>
2017-11-13 17:16:46 +0900

    commit anyway

--- CalcApp/CalcApp/CalcApp/ViewController.swift
+++ CalcApp/CalcApp/CalcApp/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
     let btnMargin:Double = 10
     let labelHeight:Double = 190
     
+    var num1:Double = 0.0
+    var num2:Double = 0.0
+    var calcResult:Double = 0.0
+    var operatorType = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -44,12 +49,17 @@ class ViewController: UIViewController {
                 //        button.frame = CGRect(x: btnX, y: btnY, width: btnWidth, height: btnHeight)
                 button.frame = CGRect(x: btnX, y: btnY, width: btnWidth, height: btnHeight)
                 button.backgroundColor = UIColor.blue
+
                 let btnNum = i * Int(numRows) + j
                 button.setTitle(btnLabel[btnNum], for: UIControlState.normal)
+                
+                button.addTarget(self, action: #selector(buttonTouched), for: UIControlEvents.touchUpInside)
+//                button.addTarget(self, action: Selector(buttonTouched(sender: UIButton)), for: UIControlEvents.touchUpInside)
                 self.view.addSubview(button)
 
             }
         }
+        
 /*
         let button = UIButton()
         let btnWidth:Double = (screenWidth - (btnMargin * (numColumns + 1))) / numColumns
@@ -62,6 +72,63 @@ class ViewController: UIViewController {
         self.view.addSubview(button)
 */
         
+    }
+
+    @objc func buttonTouched(sender: UIButton) {
+        
+        let btnTitle:String = sender.currentTitle!
+//        print("\(btnTitle)ボタンがタッチされました")
+        switch btnTitle {
+            case "0","1","2","3","4","5","6","7","8","9","0" :
+                numberTouched(btnTitle: btnTitle)
+            case "/","-","x","+" :
+                operatorTouched(btnTitle: btnTitle)
+            case "=" :
+                equalTouched(btnTitle: btnTitle)
+            default :
+                clearTouched(btnTitle: btnTitle)
+        }
+    }
+    
+    func numberTouched(btnTitle:String) {
+        print("\(btnTitle)数字がタッチされました")
+        let btnValue:Double = Double(btnTitle)!
+        num1 = num1 * 10 + btnValue
+        resultLabel.text = String(format:"%.0f", num1)
+    }
+    func operatorTouched(btnTitle:String) {
+        print("\(btnTitle)演算子がタッチされました")
+        operatorType = btnTitle
+        num2 = num1
+        num1 = 0
+    }
+    func equalTouched(btnTitle:String) {
+        print("\(btnTitle)イコールがタッチされました")
+        switch operatorType {
+            case "+" :
+                calcResult = num1 + num2
+            case "-" :
+                calcResult = num2 - num1
+            case "x" :
+                calcResult = num1 * num2
+            case "/" :
+                calcResult = num2 / num1
+            default :
+                print("その他")
+//                break
+        }
+        num1 = calcResult
+//        resultLabel.text = String(format:"%.0f", num1)
+        resultLabel.text = String("\(calcResult)")
+
+    }
+
+    func clearTouched(btnTitle:String) {
+        print("\(btnTitle)クリアがタッチされました")
+        num1 = 0
+        num2 = 2
+        operatorType = ""
+        resultLabel.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
