45593a14585fbede67ad786199c033a8d1f1369e
佐藤雅之 <sato-masay@mackbookPro.local>
2017-11-19 17:52:46 +0900

    commit anyway

---
+++ MapApp2/MapApp2/MapApp2/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  MapApp2
+//
+//  Created by Mijabi on 2017/11/13.
+//  Copyright © 2017年 Mijabi. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        inputText.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()//キーボードを閉じる
+        let searchKeyword = textField.text
+        print(searchKeyword!)//値が空でも大丈夫なようにアンラップ（!をつける）
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(searchKeyword!, completionHandler: {(placemarks: [CLPlacemark]?, error:Error?) in
+            if let placemarks = placemarks?[0] {
+                if let targetCoordinate = placemarks.location?.coordinate {
+                    print(targetCoordinate)
+                }
+            }
+        })
+        // 3-21
+        return true
+    }
+
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var displayMap: MKMapView!
+    
+}
+
---
+++ Study2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playgrounds"
+
+print(str)
+
+var a = 5
+var b = 2
+
+a + b
+
+for i in 1...9 {
+    print(i)
+}
+
+for i in 1..<9 {
+    print(i)
+}
