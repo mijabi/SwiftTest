b27e8796fb62f7a5342ba736cdc5c8035e9b8288
佐藤雅之 <sato-masay@mackbookPro.local>
2017-11-24 00:28:56 +0900

    add timerApp with multiple view-controller

--- CameraApp/CameraApp/CameraApp/ViewController.swift
+++ CameraApp/CameraApp/CameraApp/ViewController.swift
@@ -28,12 +28,29 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         // カメラが使えるかどうか判定
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             print("Camera can be used.")
+            let ipc = UIImagePickerController()
+            ipc.sourceType = .camera
+            ipc.delegate = self
+            present(ipc, animated:true, completion: nil)
+            
         } else {
             print("Camera is not available.")
         }
     }
     
     @IBAction func shareAction(_ sender: Any) {
+        // photoImage.image があれば true
+        if let sharedImage = photoImage.image {
+            let sharedItems = [sharedImage]
+            let controller = UIActivityViewController(activityItems: sharedItems, applicationActivities: nil)
+            controller.popoverPresentationController?.sourceView = view // for iPad probrem
+            present(controller, animated: true,completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        photoImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
     }
 
 }
