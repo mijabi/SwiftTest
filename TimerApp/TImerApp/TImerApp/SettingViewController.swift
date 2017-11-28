76ba382b75b74328469868a80d45e5e60e7759ec
佐藤雅之 <sato-masay@mackbookPro.local>
2017-11-28 23:53:57 +0900

    done timerApp anyway

--- TimerApp/TImerApp/TImerApp/SettingViewController.swift
+++ TimerApp/TImerApp/TImerApp/SettingViewController.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate { //UIPickerViewDataSource ：ピッカー用。
 
+    let valueArray: [Int] = [10, 30, 60, 120, 180]
+    let settingKey = "timerValue"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timerPicker.delegate = self
+        timerPicker.dataSource = self
+        
+        let settings = UserDefaults.standard
+        let timerValue = settings.integer(forKey: settingKey)
+        
+        for row in 0..<valueArray.count {
+            if valueArray[row] == timerValue {
+                timerPicker.selectRow(row, inComponent: 0, animated: true)
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -34,6 +49,25 @@ class SettingViewController: UIViewController {
 
     @IBOutlet weak var timerPicker: UIPickerView!
     @IBAction func chooseAction(_ sender: Any) {
+        _ = navigationController?.popViewController(animated: true) // 一つ前の画面に戻る関数
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return valueArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(valueArray[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let settings = UserDefaults.standard
+        settings.setValue(valueArray[row], forKey: settingKey)
+        settings.synchronize() // 再起動したりしても、値を保持しておける？
     }
     
 }
