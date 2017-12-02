d58671d7a6277c5cefa856c809ab9e465d91d87f
Mijabi <mijabi@gmail.com>
2017-12-02 17:33:13 +0900

    add imageAI app

---
+++ ImageAI/ImageAI/ImageAI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageAI
+//
+//  Created by 佐藤雅之 on 2017/12/02.
+//  Copyright © 2017年 Mijabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
